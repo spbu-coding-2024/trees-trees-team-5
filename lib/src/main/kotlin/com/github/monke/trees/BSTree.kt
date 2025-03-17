50dc375c9a65ab00636eae47635930bd85e774ea
XRenso <tch.xr@yandex.ru>
2025-03-17 09:46:01 +0300

    Fix: write fix for delete method in bst.

--- lib/src/main/kotlin/com/github/monke/trees/BSTree.kt
+++ lib/src/main/kotlin/com/github/monke/trees/BSTree.kt
@@ -3,11 +3,12 @@ package com.github.monke.trees
 import com.github.monke.nodes.BSTNode
 
 
-public class BSTree<K : Comparable<K>, V>: BinaryTree<K, V, BSTNode<K,V>>() {
+
+public class BSTree<K : Comparable<K>, V> : BinaryTree<K, V, BSTNode<K, V>>() {
 
     override fun insert(key: K, value: V) {
         val node = BSTNode(key, value)
-        if (rootNode == null){
+        if (rootNode == null) {
             rootNode = node
             return
         }
@@ -16,14 +17,14 @@ public class BSTree<K : Comparable<K>, V>: BinaryTree<K, V, BSTNode<K,V>>() {
         var parent_node = rootNode
         while (current_node != null) {
             parent_node = current_node
-            if(node.key < current_node.key){
+            if (node.key < current_node.key) {
                 current_node = current_node.leftChild
-            } else if(node.key > current_node.key){
+            } else if (node.key > current_node.key) {
                 current_node = current_node.rightChild
             }
         }
-        if (parent_node != null){
-            when{
+        if (parent_node != null) {
+            when {
                 node.key > parent_node.key -> parent_node.rightChild = node
                 else -> parent_node.leftChild = node
             }
@@ -32,7 +33,7 @@ public class BSTree<K : Comparable<K>, V>: BinaryTree<K, V, BSTNode<K,V>>() {
 
     }
 
-    private fun getMaxSubTree(node: BSTNode<K,V>?): BSTNode<K,V>? {
+    private fun getMaxSubTree(node: BSTNode<K, V>?): BSTNode<K, V>? {
         if (node == null) return null
         var current_node = node
         while (current_node?.rightChild != null) {
@@ -41,21 +42,23 @@ public class BSTree<K : Comparable<K>, V>: BinaryTree<K, V, BSTNode<K,V>>() {
         return current_node
     }
 
-    private fun searchParentNode(node: BSTNode<K,V>?): BSTNode<K,V>? {
+    private fun searchParentNode(node: BSTNode<K, V>?): BSTNode<K, V>? {
         if (node == null) return null
         var parrent_node = rootNode
         while (parrent_node != null) {
-            when{
-                (node.key < parrent_node.key && node != parrent_node.leftChild)-> parrent_node = parrent_node.leftChild
-                (node.key > parrent_node.key && node != parrent_node.rightChild) -> parrent_node = parrent_node.rightChild
+            when {
+                (node.key < parrent_node.key && node != parrent_node.leftChild) -> parrent_node = parrent_node.leftChild
+                (node.key > parrent_node.key && node != parrent_node.rightChild) -> parrent_node =
+                    parrent_node.rightChild
+
                 (node == parrent_node.leftChild || node == parrent_node.rightChild) -> return parrent_node
             }
         }
         return null
     }
 
-    private fun setChild(parrent_node: BSTNode<K,V>?, node: BSTNode<K,V>?, new_node: BSTNode<K,V>?): Boolean {
-        if (parrent_node == null || node == null || new_node == null){
+    private fun setChild(parrent_node: BSTNode<K, V>?, node: BSTNode<K, V>?, new_node: BSTNode<K, V>?): Boolean {
+        if (parrent_node == null || node == null) {
             return false
         }
         when {
@@ -66,38 +69,40 @@ public class BSTree<K : Comparable<K>, V>: BinaryTree<K, V, BSTNode<K,V>>() {
         return true
     }
 
-    override tailrec fun delete(key: K): V? {
-        if (rootNode == null){return null}
+    override fun delete(key: K): V? {
+        if (rootNode == null) {
+            return null
+        }
+
         var node = search(key)
-        if (node?.leftChild == null || node?.rightChild == null){
+
+        if (node?.leftChild == null || node?.rightChild == null) {
             val new_node = if (node?.leftChild == null) node?.rightChild else node?.leftChild
             if (node != rootNode) {
                 val parrent_node = searchParentNode(node)
 
                 setChild(parrent_node, node, new_node)
 
-                return node?.value
-            }
-            else{
+            } else {
                 rootNode = new_node
-                return node?.value
             }
-        }
-        else{
-            val max_left_subtree_node = getMaxSubTree(node.leftChild)
-            if (max_left_subtree_node == null){return null}
-            var new_node: BSTNode<K,V>? = BSTNode(max_left_subtree_node.key, max_left_subtree_node.value)
+            return node?.value
 
-            if (new_node!= null ) {
+        } else {
+            val max_left_subtree_node = getMaxSubTree(node.leftChild) ?: return null
+            this.delete(max_left_subtree_node.key)
+            val new_node: BSTNode<K, V>? = BSTNode(max_left_subtree_node.key, max_left_subtree_node.value)
+
+            if (new_node != null) {
                 new_node.rightChild = node.rightChild
                 new_node.leftChild = node.leftChild
                 if (node == rootNode)
                     rootNode = new_node
-                else{
+                else {
                     val parrent_node = searchParentNode(node)
                     setChild(parrent_node, node, new_node)
                 }
-                return delete(max_left_subtree_node.key)
+                return node.value
             }
 
         }
@@ -105,20 +110,10 @@ public class BSTree<K : Comparable<K>, V>: BinaryTree<K, V, BSTNode<K,V>>() {
         return null
     }
 
-    override operator fun iterator(): BSTNode<K,V> {
+    override operator fun iterator(): BSTNode<K, V> {
         TODO("Not implemented yet")
     }
 }
 
 
-fun main(){
-    var tree = BSTree<Int, Int>()
-    tree.insert(1,1)
-    tree.insert(2,2)
-    tree.insert(3,3)
-    tree.insert(-5,-1)
-    tree.insert(-2,-2)
-    tree.insert(-8,-3)
-//    println( tree.delete(-5))
-    println(tree.search(-5))
-}
+
