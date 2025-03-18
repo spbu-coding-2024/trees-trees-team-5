9785dc0941b54a0c0899fd574536a1141f1fbc85
lospollosenjoyer <bbh4rris@gmail.com>
2025-03-18 04:36:51 +0300

    add: insert implementation for AVL

--- lib/src/main/kotlin/com/github/monke/trees/AVLTree.kt
+++ lib/src/main/kotlin/com/github/monke/trees/AVLTree.kt
@@ -2,6 +2,7 @@ package com.github.monke.trees
 
 import com.github.monke.nodes.AVLNode
 import java.util.Stack
+import kotlin.math.max
 
 public class AVLTree<K : Comparable<K>, V> : BinaryTree<K, V, AVLNode<K, V>>() {
     override fun insert(key: K, value: V) {
@@ -14,17 +15,24 @@ public class AVLTree<K : Comparable<K>, V> : BinaryTree<K, V, AVLNode<K, V>>() {
             if (path.isEmpty()) {
                 rootNode = newNode
             } else {
-                val parentNode: AVLNode<K, V> = path.peek()
+                var parentNode: AVLNode<K, V> = path.peek()
                 if (key < parentNode.key) {
                     parentNode.leftChild = newNode
                 } else if (key > parentNode.key) {
                     parentNode.rightChild = newNode
                 }
 
-                while (path.isNotEmpty()) {
-                    val pathNode: AVLNode<K, V> = path.pop()
-                    pathNode.rebalance()
+                while (path.size > 1) {
+                    val currentNode: AVLNode<K, V> = path.pop()
+                    parentNode = path.peek()
+                    if (currentNode.key == parentNode.leftChild?.key) {
+                        parentNode.leftChild = currentNode.rebalanced()
+                    } else {
+                        parentNode.rightChild = currentNode.rebalanced()
+                    }
                 }
+                val currentNode: AVLNode<K, V> = path.pop()
+                rootNode = currentNode.rebalanced()
             }
         }
     }
@@ -39,7 +47,6 @@ public class AVLTree<K : Comparable<K>, V> : BinaryTree<K, V, AVLNode<K, V>>() {
 
         while (currentNode != null) {
             path.push(currentNode)
-
             currentNode = when {
                 key < currentNode.key -> currentNode.leftChild
                 key > currentNode.key -> currentNode.rightChild
@@ -50,13 +57,50 @@ public class AVLTree<K : Comparable<K>, V> : BinaryTree<K, V, AVLNode<K, V>>() {
         return path
     }
 
-    private fun AVLNode<K, V>.rebalance() {
-        TODO("Not yet implemented")
+    private fun AVLNode<K, V>.rebalanced(): AVLNode<K, V>? {
+        fun AVLNode<K, V>.rotatedRight(): AVLNode<K, V>? {
+            val lChild: AVLNode<K, V>? = this.leftChild
+            this.leftChild = lChild?.rightChild
+            lChild?.rightChild = this
+            this.reheight()
+            lChild?.reheight()
+            return lChild
+        }
+
+        fun AVLNode<K, V>.rotatedLeft(): AVLNode<K, V>? {
+            val rChild: AVLNode<K, V>? = this.rightChild
+            this.rightChild = rChild?.leftChild
+            rChild?.leftChild = this
+            this.reheight()
+            rChild?.reheight()
+            return rChild
+        }
+
+        this.reheight()
+        when (this.balanceFactor) {
+            2 -> {
+                val rChild: AVLNode<K, V>? = this.rightChild
+                if (rChild != null && rChild.balanceFactor < 0) {
+                    this.rightChild = rChild.rotatedRight()
+                }
+                return this.rotatedLeft()
+            }
+
+            -2 -> {
+                val lChild: AVLNode<K, V>? = this.leftChild
+                if (lChild != null && lChild.balanceFactor > 0) {
+                    this.leftChild = lChild.rotatedLeft()
+                }
+                return this.rotatedRight()
+            }
+        }
+        return this
     }
 
-    private fun AVLNode<K, V>.balanceFactor(): Int {
-        val rightHeight: Int = this.rightChild?.height ?: 0
-        val leftHeight: Int = this.leftChild?.height ?: 0
-        return rightHeight - leftHeight
+    private val AVLNode<K, V>?.balanceFactor: Int
+        get() = (this?.rightChild?.height ?: 0) - (this?.leftChild?.height ?: 0)
+
+    private fun AVLNode<K, V>.reheight() {
+        this.height = max(this.rightChild?.height ?: 0, this.leftChild?.height ?: 0) + 1
     }
 }
