303d934cfc56abff1a84a65354cfd8d21aee3be7
XRenso <tch.xr@yandex.ru>
2025-03-19 16:22:33 +0300

    Code: Write Insert by node realization by calling insert for node and for its child.

--- lib/src/main/kotlin/com/github/monke/trees/treeInterfaces/Insert.kt
+++ lib/src/main/kotlin/com/github/monke/trees/treeInterfaces/Insert.kt
@@ -2,9 +2,12 @@ package com.github.monke.trees.treeInterfaces
 
 import com.github.monke.nodes.BinaryTreeNode
 
+
 public interface Insert<K : Comparable<K>, V, N : BinaryTreeNode<K, V, N>> {
     fun insert(key: K, value: V)
     fun insert(node: N) {
         insert(node.key, node.value)
+        node.leftChild.let { if(it!= null) insert(it)}
+        node.rightChild.let { if(it!= null) insert(it)}
     }
 }
