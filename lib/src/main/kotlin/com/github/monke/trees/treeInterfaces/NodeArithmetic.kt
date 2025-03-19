d587ba792de7b802f9c5c563db62e31b8403cf4a
XRenso <tch.xr@yandex.ru>
2025-03-19 18:09:47 +0300

    Fix: change signatures for arithmetic operation.

--- lib/src/main/kotlin/com/github/monke/trees/treeInterfaces/NodeArithmetic.kt
+++ lib/src/main/kotlin/com/github/monke/trees/treeInterfaces/NodeArithmetic.kt
@@ -4,19 +4,19 @@ import com.github.monke.nodes.BinaryTreeNode
 import com.github.monke.trees.BinaryTree
 
 public interface NodeArithmetic<K : Comparable<K>, V, N : BinaryTreeNode<K, V, N>, T : BinaryTree<K, V, N, T>> {
-    operator fun plus(node: N): BinaryTree<K, V, N, T> {
+    operator fun plus(tree: T): BinaryTree<K, V, N, T> {
         TODO("Not yet implemented")
     }
 
-    operator fun minus(node: N): BinaryTree<K, V, N, T> {
+    operator fun minus(tree: T): BinaryTree<K, V, N, T> {
         TODO("Not yet implemented")
     }
 
-    operator fun plusAssign(node: N) {
+    operator fun plusAssign(tree: T) {
         TODO("Not yet implemented")
     }
 
-    operator fun minusAssign(node: N) {
+    operator fun minusAssign(tree: T) {
         TODO("Not yet implemented")
     }
 }
