7cd10b033c500763edfa651eb2e7a45b2d941320
TheFollan <a.a-t@bk.ru>
2025-03-24 20:09:32 +0300

    Added method insert

--- lib/src/main/kotlin/com/github/monke/trees/RBTree.kt
+++ lib/src/main/kotlin/com/github/monke/trees/RBTree.kt
@@ -4,7 +4,34 @@ import com.github.monke.nodes.RBTNode
 
 public class RBTree<K : Comparable<K>, V> : BinaryTree<K, V, RBTNode<K, V>, RBTree<K, V>>() {
     override fun insert(key: K, value: V) {
-        TODO("Not yet implemented")
+
+        val node = RBTNode(key, value)
+
+        if (rootNode == null){
+           rootNode = node(RBTNode.Color.BLACK)
+        }
+
+        var currentNode = rootNode
+        var parentNode = rootNode
+
+        while(currentNode != null) {
+
+            parentNode = currentNode
+
+            currentNode = when {
+                currentNode.key < node.key -> currentNode.leftChild
+                currentNode.key > node.key -> currentNode.rightChild
+                else -> throw IllegalArgumentException("Noda with key: ${node.key} is already exists")
+            }
+        }
+
+        if(parentNode != null){
+            when{
+                node.key < parentNode.key -> parentNode.leftChild = node
+                else -> parentNode.rightChild = node
+            }
+        }
+        balancinng()
     }
 
     override fun delete(key: K): V? {
