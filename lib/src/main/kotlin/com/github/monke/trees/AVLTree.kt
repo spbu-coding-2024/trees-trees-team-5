eaabfa72e57f2e20e4f6dabfe83844b66156b987
lospollosenjoyer <bbh4rris@gmail.com>
2025-03-25 06:27:35 +0300

    add: delete implementation for AVL

--- lib/src/main/kotlin/com/github/monke/trees/AVLTree.kt
+++ lib/src/main/kotlin/com/github/monke/trees/AVLTree.kt
@@ -1,67 +1,91 @@
 package com.github.monke.trees
 
 import com.github.monke.nodes.AVLNode
-import java.util.*
+import java.util.Stack
 import kotlin.math.max
+
+typealias Path<K, V> = Stack<AVLNode<K, V>>
 
 public class AVLTree<K : Comparable<K>, V> : BinaryTree<K, V, AVLNode<K, V>>() {
     override fun insert(key: K, value: V) {
-        val path: Stack<AVLNode<K, V>>? = rootNode?.searchPath(key)
+        val path: Path<K, V>? = rootNode?.searchPath(key)
 
-        val newNode: AVLNode<K, V> = AVLNode(key, value)
+        val insertedNode: AVLNode<K, V> = AVLNode(key, value)
         if (path.isNullOrEmpty()) {
-            rootNode = newNode
+            rootNode = insertedNode
         } else {
-            var parentNode: AVLNode<K, V> = path.peek()
-            if (parentNode.key == key) {
+            if (path.peek().key == key) {
                 throw IllegalArgumentException("Node with such key already exists")
             } else {
-                if (key < parentNode.key) {
-                    parentNode.leftChild = newNode
-                } else if (key > parentNode.key) {
-                    parentNode.rightChild = newNode
-                }
-
-                while (path.size > 1) {
-                    val currentNode: AVLNode<K, V> = path.pop()
-                    val newCurrentNode: AVLNode<K, V>? = currentNode.rebalanced()
-                    parentNode = path.peek()
-                    if (currentNode.key == parentNode.leftChild?.key) {
-                        parentNode.leftChild = newCurrentNode
-                    } else {
-                        parentNode.rightChild = newCurrentNode
-                    }
-                }
-                val currentNode: AVLNode<K, V> = path.pop()
-                rootNode = currentNode.rebalanced()
+                path.roadToRootNode(key, insertedNode)
             }
         }
     }
 
     override fun delete(key: K): V? {
-        val path: Stack<AVLNode<K, V>>? = rootNode?.searchPath(key)
-        if (path.isNullOrEmpty() || path.peek().key != key) {
-            throw IllegalArgumentException("Node with such key does not exist")
-        } else {
-            // deletion
+        tailrec fun minKeyNode(node: AVLNode<K, V>): AVLNode<K, V> {
+            return if (node.leftChild == null) node else minKeyNode(node.leftChild as AVLNode<K, V>)
+        }
 
-            while (path.size > 1) {
-                val currentNode: AVLNode<K, V> = path.pop()
-                val parentNode: AVLNode<K, V> = path.peek()
-                val newCurrentNode: AVLNode<K, V>? = currentNode.rebalanced()
-                if (currentNode.key == parentNode.leftChild?.key) {
-                    parentNode.leftChild = newCurrentNode
-                } else {
-                    parentNode.rightChild = newCurrentNode
-                }
+        fun deleteMinKeyNode(node: AVLNode<K, V>): AVLNode<K, V>? {
+            if (node.leftChild == null) {
+                return node.rightChild
             }
-            val currentNode: AVLNode<K, V> = path.pop()
-            rootNode = currentNode.rebalanced()
+            node.leftChild = deleteMinKeyNode(node.leftChild as AVLNode<K, V>)
+            return rebalanced(node)
+        }
+
+        val path: Path<K, V>? = rootNode?.searchPath(key)
+        if (path.isNullOrEmpty()) {
+            return null
+        } else {
+            val deletedNode: AVLNode<K, V> = path.pop()
+            if (deletedNode.key != key) {
+                return null
+            }
+
+            val lChild: AVLNode<K, V>? = deletedNode.leftChild
+            val rChild: AVLNode<K, V>? = deletedNode.rightChild
+            var replacingNode: AVLNode<K, V>?
+
+            if (rChild == null) {
+                replacingNode = lChild
+            } else {
+                replacingNode = minKeyNode(rChild)
+                replacingNode.rightChild = deleteMinKeyNode(rChild)
+                replacingNode.leftChild = lChild
+                replacingNode = rebalanced(replacingNode)
+            }
+
+            path.roadToRootNode(key, replacingNode)
+            return deletedNode.value
         }
     }
 
-    private fun AVLNode<K, V>.searchPath(key: K): Stack<AVLNode<K, V>> {
-        val path: Stack<AVLNode<K, V>> = Stack()
+    private fun Path<K, V>.roadToRootNode(key: K, newNode: AVLNode<K, V>?) {
+        var parentNode: AVLNode<K, V> = this.peek()
+        if (key < parentNode.key) {
+            parentNode.leftChild = newNode
+        } else if (key > parentNode.key) {
+            parentNode.rightChild = newNode
+        }
+
+        while (this.size > 1) {
+            val currentNode: AVLNode<K, V> = this.pop()
+            val newCurrentNode: AVLNode<K, V> = rebalanced(currentNode)
+            parentNode = this.peek()
+            if (currentNode.key == parentNode.leftChild?.key) {
+                parentNode.leftChild = newCurrentNode
+            } else {
+                parentNode.rightChild = newCurrentNode
+            }
+        }
+        val currentNode: AVLNode<K, V> = this.pop()
+        rootNode = rebalanced(currentNode)
+    }
+
+    private fun AVLNode<K, V>.searchPath(key: K): Path<K, V> {
+        val path: Path<K, V> = Path()
         var currentNode: AVLNode<K, V>? = this
 
         while (currentNode != null) {
@@ -76,50 +100,51 @@ public class AVLTree<K : Comparable<K>, V> : BinaryTree<K, V, AVLNode<K, V>>() {
         return path
     }
 
-    private fun AVLNode<K, V>.rebalanced(): AVLNode<K, V>? {
-        fun AVLNode<K, V>.rotatedRight(): AVLNode<K, V>? {
-            val lChild: AVLNode<K, V>? = this.leftChild
-            this.leftChild = lChild?.rightChild
-            lChild?.rightChild = this
-            this.reheight()
-            lChild?.reheight()
-            return lChild
+    private fun balanceFactor(node: AVLNode<K, V>): Int {
+        return (node.rightChild?.height ?: 0) - (node.leftChild?.height ?: 0)
+    }
+
+    private fun reheight(node: AVLNode<K, V>) {
+        node.height = max(node.rightChild?.height ?: 0, node.leftChild?.height ?: 0) + 1
+    }
+
+    private fun rebalanced(node: AVLNode<K, V>): AVLNode<K, V> {
+        fun rotatedRight(node: AVLNode<K, V>): AVLNode<K, V> {
+            val successor: AVLNode<K, V> = node.leftChild as AVLNode<K, V>
+            node.leftChild = successor.rightChild
+            successor.rightChild = node
+            reheight(node)
+            reheight(successor)
+            return successor
         }
 
-        fun AVLNode<K, V>.rotatedLeft(): AVLNode<K, V>? {
-            val rChild: AVLNode<K, V>? = this.rightChild
-            this.rightChild = rChild?.leftChild
-            rChild?.leftChild = this
-            this.reheight()
-            rChild?.reheight()
-            return rChild
+        fun rotatedLeft(node: AVLNode<K, V>): AVLNode<K, V> {
+            val successor: AVLNode<K, V> = node.rightChild as AVLNode<K, V>
+            node.rightChild = successor.leftChild
+            successor.leftChild = node
+            reheight(node)
+            reheight(successor)
+            return successor
         }
 
-        this.reheight()
-        when (this.balanceFactor) {
+        reheight(node)
+        when (balanceFactor(node)) {
             2 -> {
-                val rChild: AVLNode<K, V>? = this.rightChild
-                if (rChild != null && rChild.balanceFactor < 0) {
-                    this.rightChild = rChild.rotatedRight()
+                val rChild: AVLNode<K, V>? = node.rightChild
+                if (rChild != null && balanceFactor(rChild) < 0) {
+                    node.rightChild = rotatedRight(rChild)
                 }
-                return this.rotatedLeft()
+                return rotatedLeft(node)
             }
 
             -2 -> {
-                val lChild: AVLNode<K, V>? = this.leftChild
-                if (lChild != null && lChild.balanceFactor > 0) {
-                    this.leftChild = lChild.rotatedLeft()
+                val lChild: AVLNode<K, V>? = node.leftChild
+                if (lChild != null && balanceFactor(lChild) > 0) {
+                    node.leftChild = rotatedLeft(lChild)
                 }
-                return this.rotatedRight()
+                return rotatedRight(node)
             }
         }
-        return this
-    }
-
-    private val AVLNode<K, V>?.balanceFactor: Int
-        get() = (this?.rightChild?.height ?: 0) - (this?.leftChild?.height ?: 0)
-
-    private fun AVLNode<K, V>.reheight() {
-        this.height = max(this.rightChild?.height ?: 0, this.leftChild?.height ?: 0) + 1
+        return node
     }
 }
