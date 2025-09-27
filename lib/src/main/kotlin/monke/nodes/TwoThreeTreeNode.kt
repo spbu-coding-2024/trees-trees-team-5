4c8e80e927dc79e5c13eb5d6b8148e2fbc7d2bc8
TheFollan <a.a-t@bk.ru>
2025-09-28 01:12:08 +0300

    Added method insert

--- lib/src/main/kotlin/monke/nodes/TwoThreeTreeNode.kt
+++ lib/src/main/kotlin/monke/nodes/TwoThreeTreeNode.kt
@@ -8,4 +8,8 @@ public class TwoThreeTreeNode<K : Comparable<K>, V> (
     val entries: MutableList<Entry<K, V>> = mutableListOf(),
     val children: MutableList<TwoThreeTreeNode<K, V>> = mutableListOf(),
     var paretn: TwoThreeTreeNode<K,V>? = null
-)
+) {
+    val isLeaf : Boolean
+        get() = children.isEmpty()
+}
+
