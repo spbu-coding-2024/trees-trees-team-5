1e667b44a28d62ee8de337f1f156d1b8130ca3ed
TheFollan <a.a-t@bk.ru>
2025-09-27 23:07:42 +0300

    Added fun get

--- lib/src/main/kotlin/monke/nodes/TwoThreeTreeNode.kt
+++ lib/src/main/kotlin/monke/nodes/TwoThreeTreeNode.kt
@@ -1,6 +1,11 @@
 package monke.nodes
 
+data class Entry<K : Comparable<K>, V>(
+    val key: K,
+    var value: V
+)
 public class TwoThreeTreeNode<K : Comparable<K>, V> (
-    key : K,
-    value : V
-): BinaryTreeNode<K, V, TwoThreeTreeNode<K, V>> (key, value)
+    val entries: MutableList<Entry<K, V>> = mutableListOf(),
+    val children: MutableList<TwoThreeTreeNode<K, V>> = mutableListOf(),
+    var paretn: TwoThreeTreeNode<K,V>? = null
+)
